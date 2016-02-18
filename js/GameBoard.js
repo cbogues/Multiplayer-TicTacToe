ef431fe1c7d87d201bae3fab4ad2b9a5212e75c3
cbogues <YOUR-EMAIL-ADDRESS>
2016-02-18 15:40:41 -0800

    add javascript files

---
+++ js/GameBoard.js
@@ -0,0 +1,11 @@
+(function(){
+
+	angular
+			.module('tictactoeApp')
+			.factory('Gameboard', GameboardFunc);
+
+
+		
+
+
+});
---
+++ js/gameController.js
@@ -0,0 +1,15 @@
+(function(){
+	angular
+			.module('tictactoeApp')
+			.controller('gameController', gameControllerFunc);
+	
+
+	gameControllerFunc.$inject = ['GameBoard'];
+
+
+	function gameControllerFunc(GameBoard){
+		this.gameName = "Tic Tac Toe";
+		this.activeBoard = new GameBoard(9);
+	}
+
+})();
