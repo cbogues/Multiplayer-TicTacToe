1a4b7d8e29bf03de636a95e2536f06ca5a16b70a
cbogues <YOUR-EMAIL-ADDRESS>
2016-02-18 16:19:40 -0800

    initial start to game board

--- js/GameBoard.js
+++ js/GameBoard.js
@@ -2,10 +2,35 @@
 
 	angular
 			.module('tictactoeApp')
-			.factory('Gameboard', GameboardFunc);
+			.factory('Gameboard', GameBoardFunc);
+
+b
+	function GameBoardFunc(){
+		var TILE_CHANGE = ['notselected-tile', 'x-player','o-player'];
+
+		var Gameboard = function(numTiles){
+			// Define self
+
+			var self = this;
+
+			self.playerTurn = 0;
+			self.numTiles = numTiles;
+			self.tiles = new Array(numTiles);
+			self.toggleTile = toggleTile;
 
 
-		
+			// if I get this to firebase - For Player count
+			function pleaseLetThisWork(){
+
+				self.playerNumber = -1;
+
+				self.lobby = getLobby();
+			}
+
+		}
+
+
+	}
 
 
 });
